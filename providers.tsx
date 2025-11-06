6ea43956e1b5596f8c58a63c629f051d8ffded30
Techie_Steve <tuwa.1simon@gmail.com>
2025-11-06 19:45:32 +0100

    Add files via upload

---
+++ providers.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { injected } from "@wagmi/connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = createConfig({
+  chains: [mainnet],
+  transports: {
+    [mainnet.id]: http(),
+  },
+  connectors: [injected()],
+});
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
